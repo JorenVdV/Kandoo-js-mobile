9693a5b8000b981e81867c14c4647f759075b2cd
Seger de Kort <seger.dekort@student.kdg.be>
2017-03-13 09:23:18 +0100

    notifications settings

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,4 +4,5 @@
     organisation: string;
     emailAddress: string;
     settings : string;
+    mobileNotifications: boolean;
 }
--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -3,43 +3,44 @@ import {Http, Headers, RequestOptions, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
-import { User } from '../models/user';
-import { URLService } from '../providers/url-service';
+import {User} from '../models/user';
+import {URLService} from '../providers/url-service';
 
 /*
-  Generated class for the AuthService provider.
+ Generated class for the AuthService provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
 
 @Injectable()
 export class AuthService {
-  currentUser: any;
-  private headers = new Headers({'Content-Type': 'application/json'});
+    currentUser:any;
+    private headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor(private http: Http, private urlService: URLService) {}
-
-  public login(credentials){
-    if( credentials.email == null || credentials.password === null) {
-      return Observable.throw("Please insert credentials");
-    } else {
-      return this.http.post(this.urlService.getURL('login'),
-      JSON.stringify({
-        emailAddress: credentials.email,
-        password: credentials.password
-      }), {headers: this.headers})
-        .map((response:Response) => {
-          //console.log(response);
-          this.currentUser = response.json().user;
-          //console.log(this.currentUser);
-      })
+    constructor(private http:Http, private urlService:URLService) {
     }
-  }
 
-  public register(user:any) {
-    return this.http.post(this.urlService.getURL('register'), user, this.jwt());
-  }
+    public login(credentials) {
+        if (credentials.email == null || credentials.password === null) {
+            return Observable.throw("Please insert credentials");
+        } else {
+            return this.http.post(this.urlService.getURL('login'),
+                JSON.stringify({
+                    emailAddress: credentials.email,
+                    password: credentials.password
+                }), {headers: this.headers})
+                .map((response:Response) => {
+                    //console.log(response);
+                    this.setUserWithResponse(response);
+                    //console.log(this.currentUser);
+                })
+        }
+    }
+
+    public register(user:any) {
+        return this.http.post(this.urlService.getURL('register'), user, this.jwt());
+    }
 
     public getUserInfo():User {
         return this.currentUser;
@@ -74,15 +75,19 @@ export class AuthService {
                 originalPassword: oldPassword
             }), {headers: this.headers})
             .map((response:Response) => {
-                this.currentUser = response.json();
+                this.setUserWithResponse(response);
             })
     }
 
-    updateUser(user:User) {
+    public updateUser(user:User) {
         return this.http.put(this.urlService.getURL(`user/${this.currentUser._id}/update`),
             JSON.stringify(user), {headers: this.headers})
             .map((response:Response) => {
-                this.currentUser = response.json();
+                this.setUserWithResponse(response);
             })
+    }
+
+    private setUserWithResponse(response:Response) {
+        this.currentUser = response.json().user;
     }
 }
